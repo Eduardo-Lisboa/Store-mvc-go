0c9dc493cacf3daee7e046978061dbeb83ad20eb
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-11-24 13:55:53 -0300

    projeto finalizado :D

--- controllers/products.go
+++ controllers/products.go
@@ -44,3 +44,44 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", 301)
 
 }
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idProduct := r.URL.Query().Get("id")
+
+	models.DeleteProduct(idProduct)
+
+	http.Redirect(w, r, "/", 301)
+
+}
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idProduct := r.URL.Query().Get("id")
+
+	product := models.EditProduct(idProduct)
+
+	templates.ExecuteTemplate(w, "Edit", product)
+
+}
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+		price := r.FormValue("price")
+		amount := r.FormValue("amount")
+
+		priceConverted, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			log.Println("Error converting price to float64: ", err)
+		}
+		amountConverted, err := strconv.Atoi(amount)
+		if err != nil {
+			log.Println("Error converting amount to int: ", err)
+		}
+		idConverted, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Error converting id to int: ", err)
+		}
+
+		models.UpdateProduct(idConverted, name, description, priceConverted, amountConverted)
+	}
+	http.Redirect(w, r, "/", 301)
+}
--- models/products.go
+++ models/products.go
@@ -12,10 +12,14 @@ type Product struct {
 	Amount      int
 }
 
+func ConectDatabase() {
+	panic("unimplemented")
+}
+
 func SearchProducts() []Product {
 	db := db.ConectDatabase()
 
-	selectQueryAll, err := db.Query("select * from products")
+	selectQueryAll, err := db.Query("select * from products order by id asc")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -48,14 +52,10 @@ func SearchProducts() []Product {
 
 }
 
-func ConectDatabase() {
-	panic("unimplemented")
-}
-
 func InsertProduct(name string, description string, price float64, amount int) {
 	db := db.ConectDatabase()
 
-	insertQuery, err := db.Prepare("INSERT INTO products(name, description, price, amount) VALUES(?, ?, ?, ?)")
+	insertQuery, err := db.Prepare("INSERT INTO products(name, description, price, amount) VALUES($1, $2, $3, $4)")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -63,4 +63,60 @@ func InsertProduct(name string, description string, price float64, amount int) {
 	insertQuery.Exec(name, description, price, amount)
 
 	defer db.Close()
+}
+
+func DeleteProduct(idProduct string) {
+	db := db.ConectDatabase()
+
+	deleteQuery, err := db.Prepare("DELETE FROM products WHERE id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deleteQuery.Exec(idProduct)
+
+	defer db.Close()
+
+}
+
+func EditProduct(id string) Product {
+	db := db.ConectDatabase()
+
+	productData, err := db.Query("SELECT * FROM products WHERE id=$1", id)
+	if err != nil {
+		panic(err.Error())
+	}
+	productAtt := Product{}
+	for productData.Next() {
+		var id, amount int
+		var name, description string
+		var price float64
+		err = productData.Scan(&id, &name, &description, &price, &amount)
+		if err != nil {
+			panic(err.Error())
+		}
+		productAtt.Id = id
+		productAtt.Name = name
+		productAtt.Description = description
+		productAtt.Price = price
+		productAtt.Amount = amount
+
+	}
+	defer db.Close()
+	return productAtt
+
+}
+
+func UpdateProduct(id int, name string, description string, price float64, amount int) {
+	db := db.ConectDatabase()
+
+	updateQuery, err := db.Prepare("UPDATE products SET name=$1, description=$2, price=$3, amount=$4 WHERE id=$5")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	updateQuery.Exec(name, description, price, amount, id)
+
+	defer db.Close()
+
 }
